0da2adb52877b9cb7e75fa1d3935c296e173ed3f
normainobary2021 <norman.ainobushoborozi@gmail.com>
2022-10-02 06:40:04 +0300

    Add files necessary for Bookstore react project

---
+++ src/components/Category.js
@@ -0,0 +1,11 @@
+import './styles/Category.css';
+
+const Category = () => (
+  <div className="categories">
+    <button type="button" className="status-btn">
+      Coming Soon
+    </button>
+  </div>
+);
+
+export default Category;
